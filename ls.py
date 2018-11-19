b7089a9ccc68bbce3f001255f642f02902aeefd1
littleyellowbicycle <1021593619@qq.com>
2018-11-19 23:17:37 +0800

    add some prac and note file

---
+++ ls.py
@@ -0,0 +1,5 @@
+# -- coding: utf-8 --
+import os
+cwd=os.getcwd()
+res=os.listdir(cwd)
+print res
---
+++ printPrac.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+test="%r %r %r %r"
+print test %(
+	"this",
+	"is",
+	"a",
+	"test"
+)
+
+
+print """
+	we can say 
+	"this is a test"
+	lol
+	?????
+"""
+
+print "%r" %"\t"
+
+
+print "this is"
+print "a test"
+raw_input_A = raw_input("raw_input: ")
+input_A = input("input: ")
+print raw_input_A
+print input_A
